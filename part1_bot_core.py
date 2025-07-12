0b139419109816b531005c31c61487361e1919aa
Deni741 <riznispravy@gmail.com>
2025-07-12 20:59:55 +0200

    Delete part1_bot_core.py

--- part1_bot_core.py
+++
@@ -1,9 +0,0 @@
-from telegram.ext import ApplicationBuilder
-from part2_handlers import add_handlers
-import os
-
-with open("bot_token.txt", "r") as f:
-    TOKEN = f.read().strip()
-
-application = ApplicationBuilder().token(TOKEN).build()
-add_handlers(application)
